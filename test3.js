00d354cff4432d4be6ca76725d16d06532cce02c
Chris Bohlman <cbohlman@pm.me>
2022-08-19 15:17:42 -0700

    Update test3.js

--- test3.js
+++ test3.js
@@ -1,1 +1,6 @@
-document.querySelector('.customer.account')
+if (window.location.pathname === "/account") {
+  const account_section = document.querySelector('.customer.account');
+  var div = document.createElement('div');  //creating element
+  div.textContent = "Hello, World";         //adding text on the element
+  account_section.appendChild(div); 
+}
